b284f063aababde60d34bd2d1c85c4056c2c4338
Augustorresfx <augustorresfx@gmail.com>
2025-01-13 00:34:35 -0300

    deploy11

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-
+python manage.py loaddata paises.json
 #if [[ $CREATE_SUPERUSER ]];
 #then
 #python manage.py createsuperuser --no-input
