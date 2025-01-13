83a29b1a828650c389637dad752fbd6dbaee8d47
Augustorresfx <augustorresfx@gmail.com>
2025-01-13 02:56:35 -0300

    deploy18

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=100)
-    codigo = models.CharField(max_length=3, unique=True)  # Código ISO 3166-1 alpha-3
+    codigo = models.CharField(max_length=10, unique=True)  # Código ISO 3166-1 alpha-3
 
     def __str__(self):
         return self.nombre
@@ -11,7 +11,7 @@ class Pais(models.Model):
 class Matriz(models.Model):
     nombre = models.CharField(max_length=100)
     pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
-    activo = models.CharField(max_length=3, choices=[('S', 'Sí'), ('N', 'No')], default='S')
+    activo = models.CharField(max_length=10, choices=[('S', 'Sí'), ('N', 'No')], default='S')
 
 class Broker(models.Model):
     nombre = models.CharField(max_length=100)
@@ -20,11 +20,11 @@ class Broker(models.Model):
     domicilio_oficina = models.CharField(max_length=100)
     url_web = models.CharField(max_length=100)
     matriz = models.ForeignKey(Matriz, on_delete=models.CASCADE)
-    activo = models.CharField(max_length=3, choices=[('S', 'Sí'), ('N', 'No')], default='S')
+    activo = models.CharField(max_length=10, choices=[('S', 'Sí'), ('N', 'No')], default='S')
 
 class Aseguradora(models.Model):
     nombre = models.CharField(max_length=100)
     pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
     tax_id = models.CharField(max_length=100)
-    activo = models.CharField(max_length=3, choices=[('S', 'Sí'), ('N', 'No')], default='S')
+    activo = models.CharField(max_length=10, choices=[('S', 'Sí'), ('N', 'No')], default='S')
 
