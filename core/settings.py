252fc0b3c77675824328a7a27358af0c4403321f
Augustorresfx <augustorresfx@gmail.com>
2025-01-15 22:38:08 -0300

    cambios-pendientes-added-10

--- core/settings.py
+++ core/settings.py
@@ -101,19 +101,19 @@ LOGOUT_REDIRECT_URL = '/login/'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#    'default': dj_database_url.config(
-#        default='postgresql://postgres:postgres@localhost/postgres',
-#        conn_max_age=600
-#    )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+   'default': dj_database_url.config(
+       default='postgresql://postgres:postgres@localhost/postgres',
+       conn_max_age=600
+   )
+}
 
 
 # Password validation
