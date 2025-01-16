f6faab02226986e92bcbf96cfdda5cf647511419
Augustorresfx <augustorresfx@gmail.com>
2025-01-15 23:14:03 -0300

    cambios-pendientes-added-12

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='pQu!}YDjXfF~%+b,P]SxC[=EdM5z:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://nkrisk2.onrender.com', 'nkrisk2.onrender.com', 'localhost']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
