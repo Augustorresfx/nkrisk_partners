eb7c58fb3f16b4269b62ad6a1486178598e76700
Augusto Torres <92930055+Augustorresfx@users.noreply.github.com>
2025-01-16 00:06:59 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ python manage.py migrate
 python manage.py loaddata paises.json
 #if [[ $CREATE_SUPERUSER ]];
 #then
-python manage.py createsuperuser --no-input
+#python manage.py createsuperuser --no-input
 #fi
 
